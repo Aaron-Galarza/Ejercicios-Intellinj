2850600bcf7f8b4d5dff4ce147a35b150e9301af
Aaron-Galarza <aarongalarza2005@gmail.com>
2024-09-26 20:52:25 -0300

    Added ArrayList Example

---
+++ src/Arraylist/ArraylistEjemplo.java
@@ -0,0 +1,51 @@
+package Arraylist;
+
+import java.util.ArrayList;
+
+public class ArraylistEjemplo {
+    public static void main(String[] args){
+
+        Auto a1 = new Auto("123456789", "rojo");
+//        Auto a2 = new Auto("987654321", "verde");
+//        Auto a3 = new Auto("987321654", "azul");
+
+        ArrayList<Auto> misAutos = new ArrayList<>();
+
+        misAutos.add(a1);
+        System.out.println(misAutos.size());
+        System.out.println(misAutos);
+
+        System.out.println();
+        misAutos.add(new Auto("987654321", "verde"));
+        System.out.println(misAutos.size());
+        System.out.println(misAutos);
+
+        System.out.println();
+        misAutos.add(new Auto("987321654", "azul"));
+        System.out.println(misAutos.size());
+        System.out.println(misAutos);
+
+        System.out.println();
+        System.out.println("El 2do auto de la lista es: " + misAutos.get(1));
+
+        System.out.println();
+        for (int i = 0; i < misAutos.size(); i++) {
+
+            System.out.println("El auto en la posicion n° " + i + " es " + misAutos.get(i));
+
+        }
+
+        System.out.println();
+
+        misAutos.remove(a1);
+        System.out.println(misAutos.size());
+        System.out.println(misAutos);
+
+        System.out.println();
+
+        misAutos.remove(1);
+        System.out.println(misAutos.size());
+        System.out.println(misAutos);
+
+    }
+}
---
+++ src/Arraylist/Auto.java
@@ -0,0 +1,22 @@
+package Arraylist;
+
+public class Auto {
+
+    private String patente;
+    private String color;
+
+    public Auto(String patente, String color){
+
+        this.patente = patente;
+        this.color = color;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "patente='" + patente + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
